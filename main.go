b803ed7a41d55af65e0d1984ee2e24a652046978
fedor <fedor.korotkov@gmail.com>
2022-04-16 17:38:43 -0400

    Leave only builder

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"os"
 	"packer-plugin-scaffolding/builder/scaffolding"
-	scaffoldingData "packer-plugin-scaffolding/datasource/scaffolding"
-	scaffoldingPP "packer-plugin-scaffolding/post-processor/scaffolding"
-	scaffoldingProv "packer-plugin-scaffolding/provisioner/scaffolding"
 	scaffoldingVersion "packer-plugin-scaffolding/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
@@ -15,9 +12,6 @@ import (
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder("my-builder", new(scaffolding.Builder))
-	pps.RegisterProvisioner("my-provisioner", new(scaffoldingProv.Provisioner))
-	pps.RegisterPostProcessor("my-post-processor", new(scaffoldingPP.PostProcessor))
-	pps.RegisterDatasource("my-datasource", new(scaffoldingData.Datasource))
 	pps.SetVersion(scaffoldingVersion.PluginVersion)
 	err := pps.Run()
 	if err != nil {
