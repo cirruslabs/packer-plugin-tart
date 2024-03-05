16705dd7ae14f67fc774779f57769ea66d14616b
Sergei Parshev <parshev@adobe.com>
2024-03-05 13:30:53 -0500

    Fixed port int to string

--- builder/tart/step_run.go
+++ builder/tart/step_run.go
@@ -181,7 +181,7 @@ func typeBootCommandOverVNC(
 	ui.Say("Retrieved VNC credentials, connecting...")
 	ui.Message(fmt.Sprintf(
 		"If you want to view the screen of the VM, connect via VNC with the password \"%s\" to\n"+
-			"vnc://%s:%d", vncPassword, vncHost, vncPort))
+			"vnc://%s:%s", vncPassword, vncHost, vncPort))
 
 	dialer := net.Dialer{}
 	netConn, err := dialer.DialContext(ctx, "tcp", fmt.Sprintf("%s:%s", vncHost, vncPort))
