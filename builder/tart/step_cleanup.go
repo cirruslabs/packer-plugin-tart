95c1764ed1f088de1517304bc665d56f50430bb0
Fedor Korotkov <fedor.korotkov@gmail.com>
2024-03-06 11:14:54 -0500

    Cleanup VM in case of a failure (#120)
    * Cleanup VM in case of a failure
    
    * Extracted cleanup in a separate step
    
    * Improved message

---
+++ builder/tart/step_cleanup.go
@@ -0,0 +1,26 @@
+package tart
+
+import (
+	"context"
+	"github.com/hashicorp/packer-plugin-sdk/multistep"
+	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
+)
+
+type stepCleanVM struct{}
+
+func (s *stepCleanVM) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
+	return multistep.ActionContinue
+}
+
+func (s *stepCleanVM) Cleanup(state multistep.StateBag) {
+	config := state.Get("config").(*Config)
+	ui := state.Get("ui").(packersdk.Ui)
+
+	_, cancelled := state.GetOk(multistep.StateCancelled)
+	_, halted := state.GetOk(multistep.StateHalted)
+	if cancelled || halted {
+		ui.Say("Cleaning up virtual machine...")
+		cmdArgs := []string{"delete", config.VMName}
+		_, _ = TartExec(context.Background(), cmdArgs...)
+	}
+}
