6807549c359349ec82105f5d5312132b7c77e925
Nikolay Edigaryev <edigaryev@gmail.com>
2024-04-04 17:50:45 +0400

    ParseDiskUtilPlistOutput: relax the requirements w.r.t. last partition

--- builder/tart/diskutil_parser.go
+++ builder/tart/diskutil_parser.go
@@ -5,7 +5,7 @@ import (
 	"howett.net/plist"
 )
 
-const expectedLastPartitionContent = "Apple_APFS"
+const expectedPartitionContent = "Apple_APFS"
 
 type DiskUtilOutput struct {
 	AllDisksAndPartitions []DiskWithPartitions `plist:"AllDisksAndPartitions"`
@@ -22,10 +22,8 @@ type Partition struct {
 }
 
 // ParseDiskUtilPlistOutput parses "diskutil list -plist" output,
-// makes sure there's only one disk on the system and returns
-// its name and the name of the last partition, additionally
-// validating that the last partition is not a recovery one
-// (which we should've deleted for the disk expansion to work).
+// makes sure there's only one disk on the system and returns its
+// name and the name of a single APFS partition or errors otherwise.
 func ParseDiskUtilPlistOutput(input []byte) (string, string, error) {
 	var diskUtilOutput DiskUtilOutput
 
@@ -42,33 +40,27 @@ func ParseDiskUtilPlistOutput(input []byte) (string, string, error) {
 	var candidates []Candidate
 
 	for _, diskWithPartitions := range diskUtilOutput.AllDisksAndPartitions {
-		// Skip disks without partitions
-		if len(diskWithPartitions.Partitions) == 0 {
-			continue
+		for _, partition := range diskWithPartitions.Partitions {
+			if partition.Content != expectedPartitionContent {
+				continue
+			}
+
+			candidates = append(candidates, Candidate{
+				DiskName:      diskWithPartitions.DeviceIdentifier,
+				PartitionName: partition.DeviceIdentifier,
+			})
 		}
-
-		// Add a candidate if this disk's last partition is expectedLastPartitionContent
-		lastPartition := diskWithPartitions.Partitions[len(diskWithPartitions.Partitions)-1]
-
-		if lastPartition.Content != expectedLastPartitionContent {
-			continue
-		}
-
-		candidates = append(candidates, Candidate{
-			DiskName:      diskWithPartitions.DeviceIdentifier,
-			PartitionName: lastPartition.DeviceIdentifier,
-		})
 	}
 
 	if len(candidates) == 0 {
-		return "", "", fmt.Errorf("found no disks on which the last partition's \"Content\" "+
-			"is %q, make sure that the macOS is installed", expectedLastPartitionContent)
+		return "", "", fmt.Errorf("found no disks on which the partition's \"Content\" "+
+			"is %q, make sure that the macOS is installed", expectedPartitionContent)
 	}
 
 	if len(candidates) > 1 {
-		return "", "", fmt.Errorf("found more than one disk on which the last partition's \"Content\" "+
+		return "", "", fmt.Errorf("found more than one disk on which the partition's \"Content\" "+
 			"is %q, please only mount a single disk that contains APFS partitions otherwise it's hard "+
-			"to tell on which disk the macOS is installed", expectedLastPartitionContent)
+			"to tell on which disk the macOS is installed", expectedPartitionContent)
 	}
 
 	return candidates[0].DiskName, candidates[0].PartitionName, nil
