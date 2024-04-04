ebc6ff23c324fb7d08f63c8d239ebee9ab8e4a0a
Nikolay Edigaryev <edigaryev@gmail.com>
2024-04-04 18:00:26 +0400

    ParseDiskUtilPlistOutput: relax the requirements w.r.t. last partition (#146)

--- builder/tart/diskutil_parser_test.go
+++ builder/tart/diskutil_parser_test.go
@@ -70,6 +70,80 @@ func TestSingleDisk(t *testing.T) {
 	require.Equal(t, "disk0s2", partitionName)
 }
 
+func TestSingleDiskWithRecoveryPartition(t *testing.T) {
+	plistBytes := `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>AllDisks</key>
+	<array>
+		<string>disk0</string>
+		<string>disk0s1</string>
+		<string>disk0s2</string>
+		<string>disk0s3</string>
+	</array>
+	<key>AllDisksAndPartitions</key>
+	<array>
+		<dict>
+			<key>Content</key>
+			<string>GUID_partition_scheme</string>
+			<key>DeviceIdentifier</key>
+			<string>disk0</string>
+			<key>OSInternal</key>
+			<false/>
+			<key>Partitions</key>
+			<array>
+				<dict>
+					<key>Content</key>
+					<string>Apple_APFS_ISC</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s1</string>
+					<key>DiskUUID</key>
+					<string>22A4F5CC-CB5B-4EF1-B4F5-EBE9F79C0EE6</string>
+					<key>Size</key>
+					<integer>524288000</integer>
+				</dict>
+				<dict>
+					<key>Content</key>
+					<string>Apple_APFS</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s2</string>
+					<key>DiskUUID</key>
+					<string>E59D951F-E0B0-42B5-BE26-DF3611AD673C</string>
+					<key>Size</key>
+					<integer>44107001856</integer>
+				</dict>
+				<dict>
+					<key>Content</key>
+					<string>Apple_APFS_Recovery</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s3</string>
+					<key>DiskUUID</key>
+					<string>7684137C-F165-45C4-948B-681012742DAC</string>
+					<key>Size</key>
+					<integer>5368672256</integer>
+				</dict>
+			</array>
+			<key>Size</key>
+			<integer>50000000000</integer>
+		</dict>
+	</array>
+	<key>VolumesFromDisks</key>
+	<array/>
+	<key>WholeDisks</key>
+	<array>
+		<string>disk0</string>
+	</array>
+</dict>
+</plist>
+`
+
+	diskName, partitionName, err := tart.ParseDiskUtilPlistOutput([]byte(plistBytes))
+	require.NoError(t, err)
+	require.Equal(t, "disk0", diskName)
+	require.Equal(t, "disk0s2", partitionName)
+}
+
 func TestMultipleDisks(t *testing.T) {
 	plistBytes := `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
