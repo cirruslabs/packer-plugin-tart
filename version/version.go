8bc458fa9258fe6d6589d76fc994a6e46034e7f3
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2024-04-07 14:54:15 +0200

    Generate plugin version at build time based on git describe
    If the git describe is a plain tag, the resulting plugin
    version matches the tag.
    
    If there are commits on top of the most recent tag, or the
    working directory is dirty, the resulting plugin version is
    the latest plugin version with the patch version bumped,
    and the "dev" suffix added.
    
    The same logic has been applied to the goreleaser file,
    so that the releasing logic can be tested with --snapshot

--- version/version.go
+++ version/version.go
@@ -6,14 +6,9 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "1.10.0"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	Version = "" // Will be set when building
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
-	PluginVersion = version.NewPluginVersion(Version, VersionPrerelease, "")
+	PluginVersion = version.NewRawVersion(Version)
 )
